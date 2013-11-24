960d1fbdfa00b83d851c7be1374b36dbb617d540
Evan Hazlett <ejhazlett@gmail.com>
2013-11-24 05:07:27 -1000

    added conf generator ; default index generation

--- readme.md
+++ readme.md
@@ -3,9 +3,15 @@
 Munin stats aggregator and reporting
 
 * `docker build -t munin .`
-* `docker run munin`
+* `docker run -p 80 munin`
+
+With a node list:
+* `docker run -p 80 -e NODES="foo.local:127.0.0.1 bar.remote:1.2.3.4" munin`
 
 Ports
 
 * 80
 
+Environment Variables
+
+* `NODES`: Space separated list of `<name>:<host>` munin node pairs. (i.e. `foo.local:127.0.0.1 bar.remote:1.2.3.4`)
--- run.sh
+++ run.sh
@@ -1,5 +1,36 @@
 #!/bin/bash
+NODES=${NODES:-}
 
+# generate node list
+for NODE in $NODES
+do
+    NAME=`echo $NODE | cut -d ':' -f1`
+    HOST=`echo $NODE | cut -d ':' -f2`
+    cat << EOF >> /etc/munin/munin.conf
+[$NAME]
+    address $HOST
+    use_node_name yes
+
+EOF
+done
+
+# placeholder html to prevent permission error
+cat << EOF > /var/cache/munin/www/index.html
+<html>
+<head>
+  <title>Munin</title>
+</head>
+<body>
+Munin has not run yet.  Please try again in a few moments.
+</body>
+</html>
+EOF
+
+# start cron
 /usr/sbin/cron &
-
+# start local munin-node
+/usr/sbin/munin-node > /dev/null 2>&1 &
+echo "Using the following munin nodes:"
+echo $NODES
+# start apache
 /usr/sbin/apache2ctl -DFOREGROUND
